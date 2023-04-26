c3e7652e717ec52079405ad34cd6a2fce90f4181
Kaleb <Kaleb@ashbyte.com>
2023-04-26 15:23:04 -0700

    update: added pillow to install requirements

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 setup(
     name='Project Stardust Auto',
     packages=find_packages(include=['Project_Stardust']),
-    version='0.1.1',
+    version='0.1.2',
     description='Python Library to play the roblox game Project Stardust',
     author='Kaleb Schmidlkofer',
     license='MIT',
-    install_requires=['pyautogui', 'pydirectinput', 'keyboard'],
+    install_requires=['pyautogui', 'pydirectinput', 'keyboard', 'pillow'],
     
 )
